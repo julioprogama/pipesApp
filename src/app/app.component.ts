53b31c1439206f02135134221005c1efab14f384
julio cesar landazury castillo <83124737+julioprogama@users.noreply.github.com>
2023-01-07 14:50:17 -0500

    inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+  nombre:string = 'Julio';
+  mostrarNombre(){
+      this.nombre= 'Camilo';
+  }
+  borrarNombre(){
+    this.nombre='';
+  }
 }
